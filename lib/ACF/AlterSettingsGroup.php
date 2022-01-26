29bc4564edd2c6ca59f28bc6d6560f7887955dab
Viljami Hirvonen <viljami.hirvonen@geniem.com>
2022-01-26 10:35:47 +0200

    Add setting tabs

--- lib/ACF/AlterSettingsGroup.php
+++ lib/ACF/AlterSettingsGroup.php
@@ -5,7 +5,10 @@
 
 namespace TMS\Theme\Taidemuseo\ACF;
 
+use Closure;
 use Geniem\ACF\Field;
+use Geniem\ACF\Exception;
+use TMS\Theme\Base\Logger;
 
 /**
  * AlterSettingsGroup
@@ -21,6 +24,13 @@ class AlterSettingsGroup {
             [ $this, 'remove_theme_color_setting' ],
             100
         );
+
+        add_filter(
+            'tms/acf/group/fg_site_settings/fields',
+            Closure::fromCallable( [ $this, 'register_theme_tabs' ] ),
+            10,
+            2
+        );
     }
 
     /**
@@ -35,6 +45,26 @@ class AlterSettingsGroup {
             if ( $field->get_name() === 'Teeman ulkoasu' ) {
                 $field->remove_field( 'theme_color' );
             }
+        }
+
+        return $fields;
+    }
+
+    /**
+     * Register fields
+     *
+     * @param array  $fields Field group fields.
+     * @param string $key    Field group key.
+     *
+     * @return array
+     */
+    protected function register_theme_tabs( array $fields, string $key ) : array {
+        try {
+            $fields[] = new Fields\Settings\ArtworkSettingsTab( '', $key );
+            $fields[] = new Fields\Settings\ArtistSettingsTab( '', $key );
+        }
+        catch ( Exception $e ) {
+            ( new Logger() )->error( $e->getMessage(), $e->getTraceAsString() );
         }
 
         return $fields;
