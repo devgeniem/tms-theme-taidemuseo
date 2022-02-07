c7b8af51378edcb53342cade29334d5649749844
Timo Haapala <timo.haapala@geniem.com>
2022-02-07 14:57:25 +0200

    Grid typography styling

--- lib/ACF/AlterGridFieldsData.php
+++ lib/ACF/AlterGridFieldsData.php
@@ -3,15 +3,29 @@
  *  Copyright (c) 2021. Geniem Oy
  */
 
+use TMS\Theme\Base\Logger;
+
 /**
- * Alter Grid Fields block, layout data
+ * Alter Grid Fields block, layout
  */
-class AlterGridFieldsData {
+class AlterGridFields {
 
     /**
      * Constructor
      */
     public function __construct() {
+        add_filter(
+            'tms/block/grid/fields',
+            [ $this, 'alter_fields' ],
+            10,
+            2
+        );
+        add_filter(
+            'tms/acf/layout/_grid/fields',
+            [ $this, 'alter_fields' ],
+            10,
+            2
+        );
         add_filter(
             'tms/acf/block/grid/data',
             [ $this, 'alter_format' ],
@@ -22,7 +36,25 @@ class AlterGridFieldsData {
             [ $this, 'alter_format' ],
             20
         );
+    }
 
+    /**
+     * Alter fields
+     *
+     * @param array $fields Array of ACF fields.
+     *
+     * @return array
+     */
+    public function alter_fields( array $fields ) : array {
+        try {
+            $fields['repeater']->sub_fields['grid_item_custom']->sub_fields['description']->set_maxlength( 300 );
+            $fields['repeater']->sub_fields['grid_item_custom']->sub_fields['description']->set_new_lines( 'br' );
+
+        }
+        catch ( Exception $e ) {
+            ( new Logger() )->error( $e->getMessage(), $e->getTrace() );
+        }
+        return $fields;
     }
 
     /**
@@ -41,4 +73,4 @@ class AlterGridFieldsData {
     }
 }
 
-( new AlterGridFieldsData() );
+( new AlterGridFields() );
